14aa8e0f5f240f75681c2dff47962d49b0294a5d
Digita1Panda <prodigykyo@gmail.com>
2024-05-06 01:26:41 +0100

    Auto Typewriter Effect added

---
+++ app.js
@@ -0,0 +1,39 @@
+const typedTextSpan = document.querySelector('.typed-text');
+const cursor = document.querySelector('.cursor')
+
+const words = ['Frontend Developer', 'Web Developer', 'Software Developer'];
+const typingDelay = 50;
+const erasingDelay = 50;
+const newLetterDelay = 2000;
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(words.length) {
+        setTimeout(type, newLetterDelay);
+    }
+})
+
+function type() {
+    if(charIndex < words[index].length) {
+        typedTextSpan.textContent += words[index].charAt(charIndex)
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newLetterDelay);
+    }
+}
+
+function erase() {
+    if(charIndex > 0) {
+        typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        index++;
+        if(index >= words.length) {
+            index = 0;
+        }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
